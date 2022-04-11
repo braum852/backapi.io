65b119f5a61cf8b6e41e48a3da7ad7db59139a36
braum852 <julian.teo59@yahoo.com>
2022-04-11 19:43:10 +1000

    Trip DB works SQL, nxt connect to react

---
+++ app/controllers/api/v1/trips_controller.rb
@@ -0,0 +1,48 @@
+class Api::V1::TripsController < ApplicationController
+
+  def index
+    @trips = Trip.all
+
+    render json: @trips
+  end
+
+  def new
+    @trip = Trip.new
+  end
+
+  # POST /trips
+  def create
+    @trip = Trip.new(trip_params)
+
+    respond_to do |format|
+      if @trip.save
+        format.html { redirect_to trip_url(@trip), notice: "Trip was successfully created." }
+        format.json { render :show, status: :created, location: @trip }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @trip.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @trip.update(trip_params)
+        format.html { redirect_to beer_url(@trip), notice: "Trip was successfully updated." }
+        format.json { render :show, status: :ok, location: @trip }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @trip.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /trips/1
+  def destroy
+    @trip.destroy
+  end
+
+    def trip_params
+        params.require(:trip).permit(:location, :done)
+      end
+end
